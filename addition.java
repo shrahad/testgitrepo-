9f72049ac8158563375738a1c2d499be78736982
shrahad <shravankumar.katta@gmail.com>
2024-10-02 17:19:25 +0530

    added sample

---
+++ addition.java
@@ -0,0 +1,11 @@
+class addition{
+	public static void main(string[args])
+	{
+		int num1,num2,res;
+		num1 =3,
+		num2 = 4,
+		res = num1+num2;
+		system.out.println("sum of "+num1" and "+num2" is "+res);
+	}
+}
+
